d7bfaecf9dc16e43235b7fe715b876b68dbf4f9a
hhp227 <hong227@naver.com>
2019-12-31 15:36:38 +0900

    2019.12.31 commit

---
+++ Application/src/com/hhp227/application/FeedbackActivity.java
@@ -0,0 +1,13 @@
+package com.hhp227.application;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FeedbackActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+    }
+}
