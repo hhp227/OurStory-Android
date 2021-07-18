d286fc522f41860ef61ba70af550a41b37b5061b
hhp227 <hong227@naver.com>
2021-07-18 23:36:32 +0900

    리펙토리중

--- app/src/main/java/com/hhp227/application/dto/MessageItem.kt
+++ app/src/main/java/com/hhp227/application/dto/MessageItem.kt
@@ -1,51 +1,5 @@
-package com.hhp227.application.dto;
+package com.hhp227.application.dto
 
-import java.io.Serializable;
+import java.io.Serializable
 
-public class MessageItem implements Serializable {
-	private int id;
-	private String message, time;
-	private User user;
-
-	public MessageItem() {
-	}
-
-	public MessageItem(int id, String message, String time, User user) {
-		this.id = id;
-		this.message = message;
-		this.time = time;
-		this.user = user;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public String getTime() {
-		return time;
-	}
-
-	public void setTime(String time) {
-		this.time = time;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-}
+data class MessageItem(var id: Int = 0, var message: String? = null, var time: String? = null, var user: User) : Serializable
