905cc73aa4d8f705d29f505c4a6d1186bdc2f0ba
hhp227 <hong227@naver.com>
2021-09-09 22:36:33 +0900

    2021.09.09 Commit

--- app/src/main/java/com/hhp227/application/util/Utils.kt
+++ app/src/main/java/com/hhp227/application/util/Utils.kt
@@ -3,7 +3,6 @@ package com.hhp227.application.util
 import android.content.Context
 import android.text.TextUtils
 import com.hhp227.application.R
-import java.text.DateFormat
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
@@ -19,28 +18,25 @@ object Utils {
             return ""
         }
         val writeDatetime = date.time
-        val nowDate = Date()
-        val nowDatetime = nowDate.time
-        val mMilliSecs = nowDatetime - writeDatetime
-        val nSecs = getSeconds(mMilliSecs)
-        val nMins = getMinutes(mMilliSecs)
-        val nHours = getHours(mMilliSecs)
-        val nDays = getDays(mMilliSecs)
+        val nowDatetime = Date().time
+        val milliSeconds = nowDatetime - writeDatetime
+        val seconds = getSeconds(milliSeconds)
+        val minutes = getMinutes(milliSeconds)
+        val hours = getHours(milliSeconds)
+        val days = getDays(milliSeconds)
 
-        if (nDays > 1) {
+        if (days > 1) {
             val cal = Calendar.getInstance()
             // yyyy-MM-dd HH:mm:ss => "MM월 dd일"
             val sdf = SimpleDateFormat(context.resources.getString(R.string.format_date))
             return sdf.format(date)
         }
-        if (nDays > 0) return String.format("%d" + context.getString(R.string.day), nDays)
-        if (nHours > 0) return String.format("%d" + context.getString(R.string.hour), nHours)
-        if (nMins > 0) return String.format("%d" + context.getString(R.string.minute), nMins)
-        if (nSecs > 1) return String.format("%d" + context.getString(R.string.second), nSecs)
-        return if (nSecs < 2) String.format(
-            "" + context.getString(R.string.afew),
-            nSecs
-        ) else strDate
+        if (days > 0) return String.format("%d" + context.getString(R.string.day), days)
+        else if (hours > 0) return String.format("%d" + context.getString(R.string.hour), hours)
+        else if (minutes > 0) return String.format("%d" + context.getString(R.string.minute), minutes)
+        else if (seconds > 1) return String.format("%d" + context.getString(R.string.second), seconds)
+        else if (seconds < 2) return String.format("" + context.getString(R.string.afew), seconds)
+        else return strDate
     }
 
     private fun getSeconds(mMilliSecs: Long): Int {
