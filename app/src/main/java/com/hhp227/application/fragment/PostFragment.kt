5e95ba252406744a054067528063bcdf2a21e28f
hhp227 <hong227@naver.com>
2022-05-02 23:01:14 +0900

    2022.05.02 Commit

--- app/src/main/java/com/hhp227/application/fragment/PostFragment.kt
+++ app/src/main/java/com/hhp227/application/fragment/PostFragment.kt
@@ -3,6 +3,7 @@ package com.hhp227.application.fragment
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
