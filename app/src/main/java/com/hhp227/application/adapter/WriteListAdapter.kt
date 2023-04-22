4b79930e162765968e9ba100e486589c6291292d
hhp227 <hong227@naver.com>
2023-04-22 23:53:19 +0900

    2023.04.22 Commit

--- app/src/main/java/com/hhp227/application/adapter/WriteListAdapter.kt
+++ app/src/main/java/com/hhp227/application/adapter/WriteListAdapter.kt
@@ -72,16 +72,14 @@ class WriteListAdapter : ListAdapter<ListItem, WriteListAdapter.WriteViewHolder>
             }
         }
         class ImageHolder(val binding: InputContentsBinding) : WriteViewHolder(binding.root) {
-            fun bind(imageItem: ListItem.Image) {
-                with(binding) {
-                    ivPreview.load(
-                        when {
-                            imageItem.bitmap != null -> imageItem.bitmap
-                            imageItem.image != null -> URLs.URL_POST_IMAGE_PATH + imageItem.image
-                            else -> null
-                        }
-                    )
-                }
+            fun bind(imageItem: ListItem.Image) = with(binding) {
+                ivPreview.load(
+                    when {
+                        imageItem.bitmap != null -> imageItem.bitmap
+                        imageItem.image != null -> URLs.URL_POST_IMAGE_PATH + imageItem.image
+                        else -> null
+                    }
+                )
             }
 
             init {
